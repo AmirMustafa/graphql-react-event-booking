6afe35f3831d67196d894d571d18f364981e7756
Amir Mustafa <amirmustafa@Amirs-MacBook-Pro.local>
2023-08-06 22:58:17 +0530

    feat: first middleware - / - hello world

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || '5000';
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+    res.send("Hello World !");
+});
 
 app.listen(port);
 

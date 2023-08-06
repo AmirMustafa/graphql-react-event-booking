e381d4badcd05884dd59659b5bf82a4d5eaec268
Amir Mustafa <amirmustafa@Amirs-MacBook-Pro.local>
2023-08-06 23:41:53 +0530

    feat: creating graphql endpoint using express-graphql

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+
+
 const app = express();
 const port = process.env.PORT || '5000';
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-    res.send("Hello World !");
-});
+app.use('/graphql', graphqlHTTP({
+    schema: null,
+    rootValue: {}
+}));
+
+// app.get("/", (req, res, next) => {
+//     res.send("Hello World !");
+// });
 
 app.listen(port);
 
